d961ab3ec943d2d95ef7bb05af8a50bf0ced52a5
CIvanPiMa <cs.ip.mt@gmail.com>
2024-05-28 22:28:42 -0500

    feat: Add initial project structure

---
+++ src/flasked_blog/__init__.py
@@ -0,0 +1,49 @@
+"""
+# Flasked Blog
+
+Blog style website developed on flask styled with bootstrap
+
+- [Docs](https://civanpima.github.io/flasked_blog/flasked_blog.html).
+
+## Installation
+
+```shell
+pip install flasked_blog
+```
+
+## Usage
+
+...TBD
+
+## Contributing
+
+Once cloned the repo, install the pre-commit hooks:
+
+```shell
+pre-commit install --install-hooks
+```
+
+Install the library (in a virtual environment) as an editable package with the development dependencies:
+
+```shell
+pip install -e ".[dev]"
+```
+
+### Documentation
+
+- This library uses the [numpydoc](https://numpydoc.readthedocs.io/en/latest/format.html) format for the docstrings.
+- To generate the documentation, run:
+
+```shell
+pdoc src/flasked_blog --mermaid --docformat numpy
+```
+
+### Testing
+
+To run the tests:
+```shell
+behave
+```
+"""
+
+__version__ = "0.0.0"
